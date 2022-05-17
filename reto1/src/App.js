a3527b05d7e31164aa9fd0718e37317cc7d66ecc
Jorge Luis Sánchez Ocampo <jorge7@utp.edu.co>
2022-05-17 18:10:01 -0500

    reto1 repo

--- reto1/src/App.js
+++ reto1/src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let objeto = {
+		realy: 'really',
+		wierd: 'weird',
+		Alcol: 'alcohol',
+		canbio: 'Cambio',
+		corason: 'corazón',
+	};
+
+	const [value, setValue] = useState('');
+	const [words, setWords] = useState([]);
+	const handleChange = e => {
+		let newValue = e.target.value;
+		if (e.keyCode === 32) {
+			let listAux = e.target.value.split(' ');
+			let endWord = listAux[listAux.length - 1];
+			console.log('end: ', endWord);
+			if (objeto[endWord]) {
+				listAux[listAux.length - 1] = objeto[endWord];
+				newValue = listAux.join(' ');
+			}
+		}
+		setValue(newValue);
+	};
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<textarea
+					value={value}
+					onChange={e => setValue(e.target.value)}
+					onKeyDown={handleChange}
+				>
+					Hola mundo...
+				</textarea>
+			</header>
+		</div>
+	);
 }
 
 export default App;
